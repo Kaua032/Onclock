f1a1471bd8323181ffca8ae35b5e136f4978af80
Kevin Feitosa <kevin.feitosa11@outlook.com>
2023-06-03 21:55:36 -0300

    feat: add create button and auto remove task

--- js/home.js
+++ js/home.js
@@ -1,29 +1,27 @@
 const todoList = document.querySelector(".todo-list");
+const todoCreateDiv = document.querySelector(".todo-create-container");
+const inputTask = document.querySelector(".todo-create-input");
+const addTaskBtn = document.querySelector(".todo-create-button");
 
 const todos = [
   {
-    "title": "Estudar para prova",
-    "color": "red",
+    "title": "Estudar",
     "done": false
   },
   {
-    "title": "Estudar para prova",
-    "color": "red",
+    "title": "Para",
     "done": false
   },
   {
-    "title": "Estudar para prova",
-    "color": "green",
+    "title": "Prova",
     "done": false
   },
   {
-    "title": "Estudar para prova",
-    "color": "green",
+    "title": "Sim",
     "done": false
   },
   {
-    "title": "Estudar para prova",
-    "color": "green",
+    "title": "estudar para prova",
     "done": false
   }
 ];
@@ -41,19 +39,58 @@ todoList.addEventListener("click", (e) => {
   }
 });
 
+todoCreateDiv.addEventListener("click", (e) => {
+  if(e.target.className === "todo-create-header" 
+  || e.target.className === "todo-create-container" 
+  || e.target.tagName === "H3" 
+  || e.target.tagName === "IMG") {
+
+    todoCreateDiv.getAttribute("data-open") === "true" 
+    ? todoCreateDiv.setAttribute("data-open", "false")
+    : todoCreateDiv.setAttribute("data-open", "true") 
+  }
+});
+
+inputTask.addEventListener("keyup", (e) => {
+  if(e.key === "Enter") {
+    return createTodo();
+  }
+})
+
+addTaskBtn.addEventListener("click", createTodo);
+
+
 function generateTodo() {
+  if(todoList.offsetHeight > 800) {
+    todos.filter((todo, index) => todo.done === true  ? todos.splice(index, 1) : null);
+  }
+      
+
     todoList.innerHTML = "";
   
     todos.map((value, index) => {
       todoList.innerHTML += `
         <div class="task ${value.done ? "completed" : "uncompleted"}">
-          <label class="task-body ${value.color}">
-            <input type="checkbox" ${value.done ? "checked" : ""}>
+          <label class="task-body ${value.done ? "green" : "red"}">
+          <input type="checkbox" ${value.done ? "checked" : ""}>
             <p>
-              <span>${index + 1}</span> - ${value.title}
+             ${value.title}
             </p>
           </label>
         </div>
       `;
     });
   }
+
+function createTodo() {
+  if(inputTask.value.trim() === "") return;
+
+  todos.push(
+    {
+      "title" : inputTask.value,
+      "done": false
+    });
+
+    inputTask.value = "";
+    generateTodo();
+}
